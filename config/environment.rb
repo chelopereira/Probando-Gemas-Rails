b89362c2dbbffbac1dc4fd065e0e8e83bed4ad5b
chelopereira <chelopp87@gmail.com>
2016-12-22 23:51:40 -0500

    mailer smtp

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# Configurar correo gmail mediante smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'probandocorreo87@gmail.com',
+  :password => "abcabc87",
+  :domain => 'gmail.com',
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true }
